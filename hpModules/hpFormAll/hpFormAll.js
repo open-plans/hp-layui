3f9e752dd930b8fa2c2765a27eede705884a64e8
mysad <kedong529@163.com>
2018-01-29 15:56:45 +0800

    增加配置类hpconfig,兼容静态服务器请求方式get

--- hpModules/hpFormAll/hpFormAll.js
+++ hpModules/hpFormAll/hpFormAll.js
@@ -1,77 +1,75 @@
 // 表单全屏层
 // hpFormAll
-layui.define(['layer', 'element'], function(exports) {
+layui.define(['layer', 'element', 'hpConfig'], function(exports) {
 	var $ = layui.jquery;
 	var layer = layui.layer;
 	var element = layui.element;
-
+    var hpConfig = layui.hpConfig;
 	var hpFormAll = {
-		open: function(elm,url,title) {
+		open: function(elm, url, title) {
 			$(document).on('click', elm, function() {
-				//Ajax获取
-				var Ajax = false; //兼容静态服务器 post 不能请求
-					//layer.closeAll('page'); //关闭所有页面层
-					$.post(url, {}, function(str) { 
-						layer.open({
-							type: 1,
-							content: str,
-							maxmin:true,
-							shade: 0,
-							title:title||'无标题',
-							success: function(layero, index) {
-								console.log('兼容静态服务器 post 不能请求')
-								console.log(layero, index);
-								
-								layer.full(index);
-								setTimeout(function(){
-									layero.find(".layui-layer-min").show();
-									layero.find(".layui-layer-maxmin").hide()
-								},100)
-							},
-							full:function(layero){
-								console.log(11);
-							},
-							min:function(layero){
-								console.log(layero);
-								console.log(22)
-								layero.find(".layui-layer-setwin a").eq(1).show();
-							}, 
-							restore:function(layero){
-								layero.find(".layui-layer-max").hide();
-								//layero.find(".layui-layer-maxmin").show()
-								//layero.find(".layui-layer-setwin a").eq(1).removeClass('layui-layer-ico layui-layer-max')
-								//.addClass('layui-layer-ico layui-layer-max layui-layer-maxmin')
-							},
-						});
-					});
-				if(!Ajax) {
-					//layer.closeAll('page'); //关闭所有页面层
+
+				if(hpConfig.isAjaxType == 'get') {
 					$.get(url, {}, function(str) {
 						layer.open({
 							type: 1,
 							content: str,
-							maxmin:true,
+							maxmin: true,
 							shade: 0,
-							title:title||'无标题',
+							title: title || '无标题',
 							success: function(layero, index) {
-								console.log('兼容静态服务器 post 不能请求')
-								console.log(layero, index);
-								
+								//console.log('兼容静态服务器 post 不能请求')
+								//console.log(layero, index);
+
 								layer.full(index);
-								setTimeout(function(){
+								setTimeout(function() {
 									layero.find(".layui-layer-min").show();
 									layero.find(".layui-layer-maxmin").hide()
-								},100)
+								}, 100)
 							},
-							full:function(layero){
-								console.log(11);
+							full: function(layero) {
+								//console.log(11);
 							},
-							min:function(layero){
-								console.log(layero);
-								console.log(22)
+							min: function(layero) {
+								//console.log(layero);
+								//console.log(22)
 								layero.find(".layui-layer-setwin a").eq(1).show();
-							}, 
-							restore:function(layero){
+							},
+							restore: function(layero) {
+								layero.find(".layui-layer-max").hide();
+								//layero.find(".layui-layer-maxmin").show()
+								//layero.find(".layui-layer-setwin a").eq(1).removeClass('layui-layer-ico layui-layer-max')
+								//.addClass('layui-layer-ico layui-layer-max layui-layer-maxmin')
+							},
+						});
+					});
+				} else {
+					$.post(url, {}, function(str) {
+						layer.open({
+							type: 1,
+							content: str,
+							maxmin: true,
+							shade: 0,
+							title: title || '无标题',
+							success: function(layero, index) {
+								//console.log('兼容静态服务器 post 不能请求')
+								//console.log(layero, index);
+
+								layer.full(index);
+								setTimeout(function() {
+									layero.find(".layui-layer-min").show();
+									layero.find(".layui-layer-maxmin").hide()
+								}, 100)
+							},
+							full: function(layero) {
+								//console.log(11);
+							},
+							min: function(layero) {
+								//console.log(layero);
+								//console.log(22)
+								layero.find(".layui-layer-setwin a").eq(1).show();
+							},
+							restore: function(layero) {
 								layero.find(".layui-layer-max").hide();
 								//layero.find(".layui-layer-maxmin").show()
 								//layero.find(".layui-layer-setwin a").eq(1).removeClass('layui-layer-ico layui-layer-max')
@@ -80,33 +78,36 @@ layui.define(['layer', 'element'], function(exports) {
 						});
 					});
 				}
+
 			});
 
 		},
-		openOpt:function(elm,url,opt){
-				$(document).on('click', elm, function() {
-				var Ajax = false; //兼容静态服务器 post 不能请求
+		openOpt: function(elm, url, opt) {
+			$(document).on('click', elm, function() {
+				//layer.closeAll('page'); //关闭所有页面层
+
+				if(hpConfig.isAjaxType == 'get') {
 					//layer.closeAll('page'); //关闭所有页面层
-					$.post(url, {}, function(str) {
-				var param = {
-					type: 1,
-					content: str
-				}
-				$.extend(opt, param)
-				layer.open(opt);
-					});
-				if(!Ajax) {
-					//layer.closeAll('page'); //关闭所有页面层
-					$.get(url, {}, function(str) { 
-								var param = {
-					type: 1,
-					content: str,
-				}
-				$.extend(opt, param)
+					$.get(url, {}, function(str) {
+						var param = {
+							type: 1,
+							content: str,
+						}
+						$.extend(opt, param)
 						layer.open(opt);
-						console.log('兼容静态服务器 post 不能请求');
+						//console.log('兼容静态服务器 post 不能请求');
+					});
+				} else {
+					$.post(url, {}, function(str) {
+						var param = {
+							type: 1,
+							content: str
+						}
+						$.extend(opt, param)
+						layer.open(opt);
 					});
 				}
+
 			});
 		}
 
--- hpModules/hpWindow/hpWindow.js
+++ hpModules/hpWindow/hpWindow.js
@@ -1,25 +1,16 @@
 // 表单全屏层
 // hpFormAll
-layui.define(['layer', 'element', 'table'], function(exports) {
+layui.define(['layer', 'element', 'table', 'hpConfig'], function(exports) {
 	var $ = layui.jquery;
 	var layer = layui.layer;
 	var element = layui.element;
 	var table = layui.table;
-
+	var hpConfig = layui.hpConfig;
 	var hpWindow = {
 		open: function(elm, url, opt) {
 			$(document).on('click', elm, function() {
-				var Ajax = false; //兼容静态服务器 post 不能请求
-				//layer.closeAll('page'); //关闭所有页面层
-				$.post(url, {}, function(str) {
-					var param = {
-						type: 1,
-						content: str
-					}
-					$.extend(opt, param)
-					layer.open(opt);
-				});
-				if(!Ajax) {
+
+				if(hpConfig.isAjaxType == 'get') {
 					//layer.closeAll('page'); //关闭所有页面层
 					$.get(url, {}, function(str) {
 						var param = {
@@ -28,16 +19,27 @@ layui.define(['layer', 'element', 'table'], function(exports) {
 						}
 						$.extend(opt, param)
 						layer.open(opt);
-						console.log('兼容静态服务器 post 不能请求');
+						//console.log('兼容静态服务器 post 不能请求');
 					});
+				} else {
+
+					$.post(url, {}, function(str) {
+						var param = {
+							type: 1,
+							content: str
+						}
+						$.extend(opt, param)
+						layer.open(opt);
+					});
+
 				}
+
 			});
 		},
 		// 弹出列表层
-		openList: function(elm, url, dataObj,callBack, opt) {
-			
+		openList: function(elm, url, dataObj, callBack, opt) {
+
 			$(document).on('click', elm, function() {
-				var Ajax = false; //兼容静态服务器 post 不能请求
 				var param = {
 					type: 1,
 					tableDom: 'kd-table',
@@ -55,20 +57,23 @@ layui.define(['layer', 'element', 'table'], function(exports) {
 				// 合并参数
 				var paramData = $.extend(param, opt, {}) // 后者覆盖前者
 				//layer.closeAll('page'); //关闭所有页面层
-				$.post(url, dataObj, function(str) {
-					paramData.type = 1;
-					paramData.content = str;
-					layer.open(paramData);
-				});
-				if(!Ajax) {
+				// 兼容静态服务器
+				if(hpConfig.isAjaxType == 'get') {
 					//layer.closeAll('page'); //关闭所有页面层
 					$.get(url, dataObj, function(str) {
 						paramData.type = 1;
 						paramData.content = str;
 						layer.open(paramData);
-						console.log('兼容静态服务器 post 不能请求');
+						//console.log('兼容静态服务器 post 不能请求');
 					})
+				} else {
+					$.post(url, dataObj, function(str) {
+						paramData.type = 1;
+						paramData.content = str;
+						layer.open(paramData);
+					});
 				}
+
 			});
 		}
 
